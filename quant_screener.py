0b8c7b098f3846b921e8999f7680b4e479811c76
hungkjkj <hungkjkj@github.com>
2026-08-24 02:48:44 +0700

    fix(math): remove max cap of 50 on ICR

--- quant_screener.py
+++ quant_screener.py
@@ -670,9 +670,6 @@ def get_stock_report(ticker, tax_rate_fallback=0.2):
             else:
                 icr = get_row_value(df_ratio, ["Khả năng thanh toán lãi vay", "ICR"], year_str=ratio_year_str)
                 
-            if icr and icr > 50:
-                icr = 50.0
-                
             history.append({
                 'year': target_year_str,
                 'roic': roic,
@@ -754,9 +751,6 @@ def get_stock_report(ticker, tax_rate_fallback=0.2):
             elif df_ratio_q is not None and not df_ratio_q.empty:
                 icr_q = get_latest_q_value(df_ratio_q, ["Khả năng thanh toán lãi vay", "ICR"])
                 if icr_q: current_icr = float(icr_q)
-                
-            if current_icr and current_icr > 50:
-                current_icr = 50.0
                 
             cfo_ttm = get_ttm_value(df_cf_q, ["Lưu chuyển tiền tệ ròng từ các hoạt động sản xuất kinh doanh", "Lưu chuyển tiền thuần từ hoạt động kinh doanh", "Net cash flows from operating activities"]) if df_cf_q is not None else 0
             tax_rate_ttm = tax_ttm / ebt_ttm if ebt_ttm > 0 else tax_rate_fallback
